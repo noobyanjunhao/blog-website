6580bfef20fa97c8213469f1be458d26a39a8c0d
noobyanjunhao <yanaw@bc.edu>
2024-08-19 20:56:06 +0800

    update

--- mysite/settings.py
+++ mysite/settings.py
@@ -120,6 +120,4 @@ STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# https://docs.djangoproject.com
