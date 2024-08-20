acdd45766565295808eb2e94765368ae3ae5c51a
noobyanjunhao <yanaw@bc.edu>
2024-08-20 14:45:24 +0800

    Changed the HTML for the site.

--- mysite/settings.py
+++ mysite/settings.py
@@ -120,4 +120,6 @@ STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
-# https://docs.djangoproject.com
+# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
